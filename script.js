5af20c60d36bd892990548c3fbd6f6fa9cbbb100
Logan Edwards <ledwards@robellum.net>
2026-06-25 15:08:40 -0500

    Overhaul how the game is interacted with
    Now, it is handled on the webpage instead of through the console

--- script.js
+++ script.js
@@ -1,50 +1,72 @@
-let strHuman;
-let strComputer;
+let playerChoice;
+let computerChoice;
 
-let humanChoice = 0;
-let computerChoice = 0;
+const choices = ['rock', 'paper', 'scissors'];
 
-let humanScore = 0;
+let count = 0;
+let playerScore = 0;
 let computerScore = 0;
+let roundMax = 5;
 
-function getHumanChoice() {
-  strHuman = prompt("Please make a choice: Rock, Paper, or Scissors.")
-  switch (strHuman) {
-    case "Rock":
-      humanChoice = 0;
-      break;
-    case "Paper":
-      humanChoice = 1;
-      break;
-    case "Scissors":
-      humanChoice = 2;
-      break;
+let result = "";
+
+function getButtons(bool) {
+  const buttons = document.getElementsByClassName("choice-button");
+  for (let el of buttons) {
+    el.disabled = bool;
   }
 }
 
-function getComputerChoice(max) {
-  return Math.floor(Math.random() * max);
+function restartGame() {
+  count = 0;
+  playerScore = 0;
+  computerScore = 0;
+
+  result = "New Game!"
+
+  document.getElementById('round-count').textContent = "Round: " + count;
+  document.getElementById('player-choice').textContent = playerChoice;
+  document.getElementById('computer-choice').textContent = computerChoice;
+  document.getElementById('player-score').textContent = "Player Score: " + playerScore;
+  document.getElementById('computer-score').textContent = "Computer Score: " + computerScore;
+  document.getElementById('round-result').textContent = result;
+  document.getElementById('round-max').textContent = "Max Rounds: " + roundMax;
+
+  getButtons("");
 }
 
-switch (getComputerChoice(3)) {
-  case 0:
-    computerChoice = 0;
-    strComputer = "Rock";
-    break;
-  case 1:
-    computerChoice = 1;
-    strComputer = "Paper";
-    break;
-  case 2:
-    computerChoice = 2;
-    strComputer = "Scissors";
-    break;
+function playGame(playerChoice) {
+  const randomIndex = Math.floor(Math.random() * 3);
+  const computerChoice = choices[randomIndex];
+
+  if (count < roundMax) {
+    if (playerChoice === computerChoice) {
+      result = "You tie this round!"
+    } else if (
+      (playerChoice === "rock" && computerChoice == "scissors") ||
+      (playerChoice === "paper" && computerChoice == "rock") ||
+      (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+      result = "You win this round!"
+      playerScore += 1;
+    } else {
+      result = "You lose this round!"
+      computerScore += 1;
+    }
+    count += 1;
+
+    document.getElementById('round-count').textContent = "Round: " + count;
+    document.getElementById('player-choice').textContent = playerChoice;
+    document.getElementById('computer-choice').textContent = computerChoice;
+    document.getElementById('player-score').textContent = "Player Score: " + playerScore;
+    document.getElementById('computer-score').textContent = "Computer Score: " + computerScore;
+    document.getElementById('round-result').textContent = result;
+    document.getElementById('round-max').textContent = "Max Rounds: " + roundMax;
+  } else {
+    result = "Game Over!"
+    document.getElementById('round-result').textContent = result;
+
+    getButtons("true");
+  }
 }
 
-function playRound(humanChoice, computerChoice) {
-    console.log("Player: " + humanChoice + ", " + strHuman);
-    console.log("Computer: " + computerChoice + ", " + strComputer);
-}
-
-getHumanChoice();
-playRound(humanChoice, computerChoice);
